10c00ecc05f63666c918910dca95e223958a9e44
roksslee <jdowkkin@gmail.com>
2023-01-14 18:42:35 -0800

    added backend part

--- src/pages/JobDetails.js
+++ src/pages/JobDetails.js
@@ -1,6 +1,20 @@
+import { useState } from "react";
+
 function JobDetail(props){
+    const [flag, setFlag] = useState();
+    let apply = () => {
+        if(flag){
+            // redirect to apply page
+        }else{
+            // redirect to login page
+        }
+    }
+    /*
+        get info from database and display
+    */
     return (
         <>
+            <button type="button" onClick={apply}>Apply</button>
         </>
     )
 }
