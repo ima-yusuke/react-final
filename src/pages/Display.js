b7ad5c756e8286c554ff4fe3e7a1bbe612be8988
roksslee <jdowkkin@gmail.com>
2023-01-16 15:59:31 -0800

    tested display component

--- src/pages/Display.js
+++ src/pages/Display.js
@@ -9,29 +9,15 @@ function Display(){
         jsonSrv.get('getJob.php')
         .then(res=>{
             setJob(res.data);
-            // console.log(jobs)
-            // console.log(res)
         })
         .catch(err=>{
             console.log(err)
         })
     }
-
-
-    // useEffect(()=>{
-    //     display();
-    //     // console.log(jobs);
-    //     // jobs.forEach((val)=>{
-    //     //     let li = document.createElement('li')
-    //     //     li.innerHTML(val)
-    //     //     document.querySelector("ul").appendChild(li)
-    //     // })
-    // },[])
-
     useEffect(()=>{
-        display()
-    },[]
-    )
+        display();
+        console.log(jobs);
+    },[])
 
     return(
         <>
